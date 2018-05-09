87cd21757742f283bf389535267142abaa0e8749
CarinaSchutte <carinaschutte@hotmail.com>
2018-05-09 13:32:01 +0200

    it is working

--- assignment_4/assignment4/assignment4-solution-starter/easier/script1.js
+++ assignment_4/assignment4/assignment4-solution-starter/easier/script1.js
@@ -63,7 +63,6 @@ for (var i = 0; i < names.length; i++) {
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak = 1
     helloSpeaker.speak(names[i]);
     
   }
